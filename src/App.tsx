c3bdbb5a1982c873d6b2949de0ba1d483571f41b
lixinyue <lixinyue@shimo.im>
2020-11-08 22:48:56 +0800

    feat: add weekpicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+
 import "./App.css";
 import { Container } from "./Container";
 
 function App() {
-  return <Container />;
+  return <MuiPickersUtilsProvider utils={DateFnsUtils}><Container /></MuiPickersUtilsProvider>;
 }
 
 export default App;
--- src/Container.tsx
+++ src/Container.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { getWeek } from 'date-fns';
 import { DataGrid, ColDef, CellParams } from '@material-ui/data-grid';
+import { DatePicker } from '@material-ui/pickers';
 
 import { responseData } from './__mock__/shift';
 import { WEEKDAYS } from './consts';
@@ -67,13 +69,28 @@ const columns: ColDef[] = [
  * 数据类型和结构都可以按照实际需求进行调整
  */
 export const Container: React.FC<Props> = ({}) => {
-  // 这里假装从 api 获取数据
-  const rows = Object.keys(responseData).map((key) => {
-    const data = responseData[key];
-    return {
-      id: key,
-      ...data,
-    };
-  });
-  return <DataGrid columns={columns} rows={rows} hideFooter />;
+  const [date, setDate] = useState(new Date());
+  // 因为每次只展示一周，获取当前 WoY 作为默认展示的周
+  const week = useMemo(() => getWeek(date), [date]);
+  // 这里假装从 api 获取 week state 对应的数据
+  // const responseData = useApi(`/weekShift/${week}`)
+  const rows = useMemo(
+    () =>
+      Object.keys(responseData).map((key) => {
+        const data = responseData[key];
+        return {
+          id: key,
+          ...data,
+        };
+      }),
+    [],
+  );
+  return (
+    <div style={{ height: '100%' }}>
+      {/* 参考这个 Demo 改成 week picker */}
+      {/* https://material-ui-pickers.dev/demo/datepicker#customization */}
+      <DatePicker value={date} onChange={setDate as any} />
+      <DataGrid columns={columns} rows={rows} hideFooter />
+    </div>
+  );
 };
